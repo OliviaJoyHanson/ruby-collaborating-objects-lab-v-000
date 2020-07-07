ae566f801ee9a5459fdcee202edbe26f62f5eb0e
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-07-07 14:23:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class Song
-  attr_accessor :name, :artist, :genre, :artist_name
+  attr_accessor :name, :artist, :mp3_importer
   @@all = []
 
   def initialize(name)
@@ -12,13 +12,13 @@ class Song
     @@all
   end
 
-  def artist_name=(artist_name)
+  def artist_name(artist_name)
     self.artist = Artist.find_or_create_by_name(artist_name)
     artist.add_song(self)
   end
 
-  def self.new_by_filename(file_name)
-    file_array = file_name.chomp(".mp3").split(" - ")
+  def self.new_by_filename(file)
+    file_array = file.chomp(".mp3").split(" - ")
     song = self.new(file_array[1])
     song.artist_name(file_array[0])
     song
