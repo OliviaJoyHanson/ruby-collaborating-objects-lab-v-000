40a7fc4421bb4a5e98854b127bcf0af721bcea2e
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-07-07 14:18:23 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,7 +8,7 @@ class MP3Importer
   end
 
   def files
-    Dir.glob("#{self.path}/*.mp3").collect {|file| file.gsub("#{@path}", "")}
+    Dir.glob("#{self.path}/*.mp3").collect {|file| file.gsub("./spec/fixtures/mp3s/", "")}
   end
 
   def import
