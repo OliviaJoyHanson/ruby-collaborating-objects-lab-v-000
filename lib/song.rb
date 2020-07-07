e862aa86c6a5572ad181b8c5a984c865c6146550
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-07-07 15:21:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,8 +19,8 @@ class Song
     song
   end
 
-  def artist_name(artistname)
-    self.artist = Artist.find_or_create_by_name(artistname)
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
 
