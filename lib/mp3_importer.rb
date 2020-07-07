ca94d03f68a0077f5c3fd2b7747d2c5859eb71f6
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-07-07 13:46:34 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,20 +1,20 @@
 require 'pry'
 
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, :files
 
   def initialize(path)
     @path = path
   end
 
   def files
-    Dir.glob("@path") {|filename|
-      filename
-    }
+    @files = Dir.glob("@path") {|filename| filename}
   end
 
   def import
-
+    @files.each do |file|
+      Song.new_by_filename(file)
+    end
   end
 
 
