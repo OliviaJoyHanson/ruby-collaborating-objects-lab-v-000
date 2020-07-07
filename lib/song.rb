8487b7e57a45e6f6f45532273cfe644db509a837
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-07-07 15:15:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,16 +12,16 @@ class Song
     @@all
   end
 
-  def artist_name=(artist_name)
-    self.artist = Artist.find_or_create_by_name(artist_name)
-    artist.add_song(self)
-  end
-
   def self.new_by_filename(file)
     file_array = file.chomp(".mp3").split(" - ")
     song = self.new(file_array[1])
     song.artist_name(file_array[0])
     song
+  end
+
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(self)
   end
 
 
