192c1908d5562ebeb8f7c7ac1d3273364afe6437
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-07-07 14:16:21 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,11 +8,11 @@ class MP3Importer
   end
 
   def files
-    Dir.glob(@path){|filename| filename}
+    Dir.glob("#{self.path}/*.mp3").collect {|file| file.gsub(@path, "")}
   end
 
   def import
-    @files.each do |file|
+    files.each do |file|
       Song.new_by_filename(file)
     end
   end
